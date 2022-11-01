e833391a426c02f1d2353a98720535a50f5ca428
Joshua Morony <joshua.morony@gmail.com>
2022-11-02 09:38:31 +1030

    async pipe example

--- src/app/home/ui/component-two.component.ts
+++ src/app/home/ui/component-two.component.ts
@@ -7,13 +7,20 @@ import { CoolButtonComponent } from 'src/app/shared/ui/cool-button.component';
   imports: [CoolButtonComponent, NgIf],
   template: `
     <ng-container *ngIf="logChangeDetection()"></ng-container>
-    <button (click)="({})">ComponentTwo</button>
+    <button (click)="increment()">ComponentTwo</button>
     <p>Component two</p>
+    {{ count }}
     <app-cool-button></app-cool-button>
   `,
   standalone: true,
 })
 export class ComponentTwo {
+  public count = 0;
+
+  increment() {
+    this.count++;
+  }
+
   logChangeDetection() {
     console.log('ComponentTwo rendered');
     return true;
